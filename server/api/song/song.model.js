f7191caa6be147e4591704189b69dada66313301
KamillaKhabibrakhmanova <venessiel@gmail.com>
2015-01-29 18:34:22 -0500

    added require for song title, source, link

--- server/api/song/song.model.js
+++ server/api/song/song.model.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var SongSchema = new Schema({
-  title: String,
+  title: { type: String, required: true },
   artist: String,
-  link: String,
-  source: String
+  link: {type: String, required: true },
+  source: {type: String, required: true }
 });
 
 module.exports = mongoose.model('Song', SongSchema);
