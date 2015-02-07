874eacf098d035b1470eef6dc68b5939f4891e34
Lindsay <ilinds4@aol.com>
2015-02-07 17:26:00 -0500

    soundcloud and tumblr streamable

--- client/app/queue/queue.controller.js
+++ client/app/queue/queue.controller.js
@@ -5,6 +5,11 @@ angular.module('splytApp')
 
 
   $scope.songs = playlist.songs;
+
+  $scope.play = function(song) {
+    console.log('kamilla pls', song)
+  }
+
   $scope.playlist = playlist;
 
   $scope.playlist_tabs=[];
@@ -16,11 +21,26 @@ angular.module('splytApp')
     $state.go('queue',{playlist_id: id}, true);
   }
 
-  var player = new Audio('/api/youtubes/stream/at3FPJaAwoY')
-  player.preload = 'metadata';
-  player.play();
-  player.controls = true;
-  document.body.appendChild(player);
+  // var player = new Audio('/api/youtubes/stream/at3FPJaAwoY')
+  // player.preload = 'metadata';
+  // player.play();
+  // player.controls = true;
+  // document.body.appendChild(player);
+
+  // var player = new Audio('/api/youtubes/stream/at3FPJaAwoY')
+  // player.preload = 'metadata';
+  // player.play();
+  // player.controls = true;
+  // document.body.appendChild(player);
+  SC.get("/tracks/182687718", {}, function(sound, error) {
+    $('#scplayer').attr('src', sound.stream_url + '?client_id=7af759eb774be5664395ed9afbd09c46');
+  });
+
+  var player2 = new Audio('https://www.tumblr.com/audio_file/uncaught/101386977846/tumblr_ne5ykbG3y11qflbpe?play_key=e6ba8f023e92bbb5aaf06052cd0c6551&tumblelog=uncaught&post_id=101386977846')
+  player2.preload = 'metadata';
+  player2.play();
+  player2.controls = true;
+  document.body.appendChild(player2);
 
   var playlistPromise = manage.getPlaylists();
 
