3c248fa428d320480ac6be32fea2de3fc57f0c66
Lindsay <ilinds4@aol.com>
2015-02-07 19:36:37 -0500

    fixed transformations bugs

--- server/api/song/song.model.js
+++ server/api/song/song.model.js
@@ -9,48 +9,49 @@ var SongSchema = new Schema({
   tag: {type: String },
   title: { type: String, required: true },
   artist: String,
-  link: {type: String, required: true },
+  link: { type: String },
+  audioSource: { type: String, default: null },
   source: {type: String, required: true }
 });
 
 //obj for returning song objects in proper format according to song source
 var transformations = {
   newYoutubeSong: function(song_obj) {
-    if (song_obj.args.info.items[0].snippet.categoryId == "10") {
-      return {
-        tag: song_obj.args.info.items[0].id,
-        title: song_obj.args.info.items[0].snippet.title.split(' - ')[1],
-        artist: song_obj.args.info.items[0].snippet.title.split(' - ')[0],
-        link: song_obj.args.song.permalink_url,
-        source: 'YouTube'
-      }
-    } else {
-      console.log("Youtube category is not music");
+    return {
+      tag: song_obj.args.info.items[0].id,
+      title: song_obj.args.info.items[0].snippet.title.split(' - ')[1],
+      artist: song_obj.args.info.items[0].snippet.title.split(' - ')[0],
+      link: song_obj.args.song.permalink_url,
+      source: 'YouTube'
     }
   },
    newSCSong: function(song_obj) {
     return {
-      tag: song_obj.song.args.song.id,
-      title: song_obj.song.args.song.title,
-      artist: song_obj.song.args.song.user.username,
-      link: song_obj.song.args.song.permalink_url,
+      tag: song_obj.args.song.id,
+      title: song_obj.args.song.title,
+      artist: song_obj.args.song.user.username,
+      link: song_obj.args.song.permalink_url,
       source:'SoundCloud'
     }
   },
    newSpotifySong: function(song_obj) {
     return {
-      tag: song_obj.song.args.info.id,
-      title: song_obj.song.args.song.title,
-      artist: song_obj.song.args.info.artists[0].name,
-      link: song_obj.song.args.song.permalink_url,
-     source: 'Spotify'
+      tag: song_obj.args.info.id,
+      title: song_obj.args.song.title,
+      artist: song_obj.args.info.artists[0].name,
+      link: song_obj.args.song.permalink_url,
+      source: 'Spotify'
     }
   },
    newTumblrSong: function(song_obj) {
+    console.log('songobj in new tumblr song', song_obj)
+    console.log('**************')
+    console.log('songobj args in new tumblr song', song_obj.args)
     return {
-      title: song_obj.song.args.song.title,
-      artist: song_obj.song.args.song.artist,
-      link: song_obj.song.args.song.permalink_url,
+      title: song_obj.args.song.title,
+      artist: song_obj.args.song.artist,
+      link: song_obj.args.song.permalink_url,
+      audioSource: song_obj.args.iframeSrc,
       source: 'Tumblr'
     }
   }
@@ -61,8 +62,9 @@ SongSchema.statics.createSong = function(song_obj) {
 
   var Song = this;
   var song_data = transformations[song_obj.song.action](song_obj.song);
+  console.log('song_data', song_data)
   Song.create(song_data, function(err, data) {
-    console.log(data);
+    console.log('data in song.create', data);
     Playlist.addNewSong(data, song_obj.playlist, song_obj.userid, function(err, model) {
       if (err)console.log(err);
     });
