c1327303586fc0a6ef01086438da9902db5427fd
Lindsay <ilinds4@aol.com>
2015-02-09 18:07:35 -0500

    been playing with the player all day, still trying to incorporate youtube and sc widgets

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('splytApp')
   .controller('MainCtrl', function ($scope, $http, socket, youtube, Auth, $sanitize, $sce) {
-    var ext_id = "gdhknebfhjejaifmglhjnpmilklnkjlg";
+    var ext_id = "fccjgnomcnlfiedbadofibbhilpbdjpl";
 
     $scope.awesomeThings = [];
     $scope.currentUser = Auth.getCurrentUser();
--- client/app/queue/queue.controller.js
+++ client/app/queue/queue.controller.js
@@ -3,164 +3,257 @@
 angular.module('splytApp')
   .controller('QueueCtrl', function ($http, playlist, $scope, youtube, $sanitize, $sce, manage, $log, $stateParams, $state) {
 
+    //youtube iframe api include
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    ///
 
-  $scope.songs = playlist.songs;
+    $scope.playlist = playlist;
+    $scope.playlist_tabs=[];
+    $scope.tabs = $scope.playlist_tabs;
 
-  $scope.play = function(song) {
-    if(song.source == 'SoundCloud') {
-      console.log('in here?', song.tag)
-       SC.get("/tracks/" + song.tag, {}, function(sound, error) {
-        $('#music').attr('src', sound.stream_url + '?client_id=7af759eb774be5664395ed9afbd09c46');
-        console.log('music source', $('#music').attr('src'))
-      });
+    //Updating playlist songs when user clicks on new tab
+    $scope.update_songs = function(id) {
+      console.log('update_songs');
+      $state.go('queue', { playlist_id: id }, true);
     }
-    if(song.source == 'YouTube') {
-      console.log('in here?', song.tag)
-      $('#music').attr('src', '/api/youtubes/stream/' + song.tag);
+
+    var playlistPromise = manage.getPlaylists();
+
+    //Getting playlists
+    playlistPromise.success(function(playlists) {
+      $scope.playlists = [];
+      for (var i = 0; i < playlists.length; i++) {
+        $scope.playlist_tabs.push(playlists[i]);
+        if (playlists[i].aggregate_stream === true) {
+          $scope.main_playlist = playlists[i];
+
+        } if (playlists[i].friend_stream === true) {
+          $scope.friend_playlist = playlists[i];
+        }
+      }
+    }).then(function(){
+      console.log($scope.playlist_tabs);
+    });
+
+    $scope.songs = playlist.songs.map(function(song) { song.playing = 'play_arrow'; return song; });
+
+
+
+    //////////////////////////////////////////////
+    /////////// PLAYER FUNCTIONALITY /////////////
+    //////////////////////////////////////////////
+
+    var music = document.getElementById('music'); // id for audio element
+    var duration; // Duration of audio clip
+    var pButton = document.getElementById('pButton'); // play button
+    var playhead = document.getElementById('playhead'); // playhead
+    var timeline = document.getElementById('timeline'); // timeline
+    var timelineWidth = timeline.offsetWidth - playhead.offsetWidth; // timeline width adjusted for playhead
+    music.addEventListener("timeupdate", timeUpdate, false); // timeupdate event listener
+    timeline.addEventListener("click", function (event) { //Makes timeline clickable
+      moveplayhead(event);
+      music.currentTime = duration * clickPercent(event);
+    }, false);
+
+    function clickPercent(e) { // returns click as decimal (.77) of the total timelineWidth
+      return (e.pageX - timeline.offsetLeft) / timelineWidth;
     }
-    if(song.source == 'Tumblr') {
-      $('#music').attr('src', song.audioSource)
+
+    // Makes playhead draggable
+    playhead.addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+
+    // Boolean value so that mouse is moved on mouseUp only when the playhead is released
+    var onplayhead = false;
+
+    function mouseDown() {
+      onplayhead = true;
+      window.addEventListener('mousemove', moveplayhead, true);
+      music.removeEventListener('timeupdate', timeUpdate, false);
     }
-    // $scope.playTrack();
-  }
 
-  // var player = new Audio('/api/youtubes/stream/at3FPJaAwoY')
-  // player.preload = 'metadata';
-  // player.play();
-  // player.controls = true;
-  // document.body.appendChild(player);
+    function mouseUp(e) {
+      if (onplayhead == true) {
+        moveplayhead(e);
+        window.removeEventListener('mousemove', moveplayhead, true);
+        // change current time
+        music.currentTime = duration * clickPercent(e);
+        music.addEventListener('timeupdate', timeUpdate, false);
+      }
+      onplayhead = false;
+    }
 
-  $scope.playlist = playlist;
-
-  $scope.playlist_tabs=[];
-
-  $scope.tabs = $scope.playlist_tabs;
-
-//Updating playlist songs when user clicks on new tab
-  $scope.update_songs = function(id) {
-    console.log('update_songs');
-    $state.go('queue', { playlist_id: id }, true);
-  }
-
-  // var player2 = new Audio('https://www.tumblr.com/audio_file/uncaught/101386977846/tumblr_ne5ykbG3y11qflbpe?play_key=e6ba8f023e92bbb5aaf06052cd0c6551&tumblelog=uncaught&post_id=101386977846')
-  // player2.preload = 'metadata';
-  // player2.play();
-  // player2.controls = true;
-  // document.body.appendChild(player2);
-
-  var playlistPromise = manage.getPlaylists();
-
-  //Getting playlists
-  playlistPromise.success(function(playlists) {
-    $scope.playlists = [];
-    for (var i = 0; i < playlists.length; i++) {
-      $scope.playlist_tabs.push(playlists[i]);
-      if (playlists[i].aggregate_stream === true) {
-        $scope.main_playlist = playlists[i];
-
-      } if (playlists[i].friend_stream === true) {
-        $scope.friend_playlist = playlists[i];
+    // Moves playhead as user drags
+    function moveplayhead(e) {
+      var newMargLeft = e.pageX - timeline.offsetLeft;
+      if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+        playhead.style.marginLeft = newMargLeft + "px";
+      }
+      if (newMargLeft < 0) {
+        playhead.style.marginLeft = "0px";
+      }
+      if (newMargLeft > timelineWidth) {
+        playhead.style.marginLeft = timelineWidth + "px";
       }
     }
-  }).then(function(){
-    console.log($scope.playlist_tabs);
-  });
 
-  var music = document.getElementById('music'); // id for audio element
-  var duration; // Duration of audio clip
-  var pButton = document.getElementById('pButton'); // play button
-
-  var playhead = document.getElementById('playhead'); // playhead
-
-  var timeline = document.getElementById('timeline'); // timeline
-  // timeline width adjusted for playhead
-  var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
-
-  // timeupdate event listener
-  music.addEventListener("timeupdate", timeUpdate, false);
-
-  //Makes timeline clickable
-  timeline.addEventListener("click", function (event) {
-    moveplayhead(event);
-    music.currentTime = duration * clickPercent(event);
-  }, false);
-
-  // returns click as decimal (.77) of the total timelineWidth
-  function clickPercent(e) {
-    return (e.pageX - timeline.offsetLeft) / timelineWidth;
-  }
-
-  // Makes playhead draggable
-  playhead.addEventListener('mousedown', mouseDown, false);
-  window.addEventListener('mouseup', mouseUp, false);
-
-  // Boolean value so that mouse is moved on mouseUp only when the playhead is released
-  var onplayhead = false;
-  // mouseDown EventListener
-  function mouseDown() {
-    onplayhead = true;
-    window.addEventListener('mousemove', moveplayhead, true);
-    music.removeEventListener('timeupdate', timeUpdate, false);
-  }
-  // mouseUp EventListener
-  // getting input from all mouse clicks
-  function mouseUp(e) {
-    if (onplayhead == true) {
-      moveplayhead(e);
-      window.removeEventListener('mousemove', moveplayhead, true);
-      // change current time
-      music.currentTime = duration * clickPercent(e);
-      music.addEventListener('timeupdate', timeUpdate, false);
+    // Synchronizes playhead position with current point in audio
+    function timeUpdate() {
+      var playPercent;
+      youtubePlaying ? playPercent = timelineWidth * ($scope.current()/duration) : playPercent = timelineWidth * (music.currentTime / duration);
+      playhead.style.marginLeft = playPercent + "px";
+      if (music.currentTime == duration) {
+        pButton.className = "";
+        pButton.className = "play";
+      }
     }
-    onplayhead = false;
-  }
-  // mousemove EventListener
-  // Moves playhead as user drags
-  function moveplayhead(e) {
-    var newMargLeft = e.pageX - timeline.offsetLeft;
-    if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
-      playhead.style.marginLeft = newMargLeft + "px";
-    }
-    if (newMargLeft < 0) {
-      playhead.style.marginLeft = "0px";
-    }
-    if (newMargLeft > timelineWidth) {
-      playhead.style.marginLeft = timelineWidth + "px";
-    }
-  }
 
-  // timeUpdate
-  // Synchronizes playhead position with current point in audio
-  function timeUpdate() {
-    var playPercent = timelineWidth * (music.currentTime / duration);
-    playhead.style.marginLeft = playPercent + "px";
-    if (music.currentTime == duration) {
-      pButton.className = "";
-      pButton.className = "play";
-    }
-  }
+    $scope.trustSrc = $sce.trustAsResourceUrl;
+    $scope.chosenIcon = 'play_arrow';
+    $scope.notStreamable = false;
+    var player, youtubePlaying = false;
 
-  //Play and Pause
-  $scope.playTrack = function() {
-    // start music
-    if (music.paused) {
-      music.play();
-      // remove play, add pause
+    var currentlyPlaying;
+    $scope.play = function(song, idx) {
+      currentlyPlaying = song;
+      song.playing !== 'pause' ? song.playing = 'pause' : song.playing = 'play_arrow';
+
+      if(youtubePlaying) {
+        youtubePlaying = false;
+        toggleYoutube();
+      }
+
+      if(song.source == 'SoundCloud') {
+        SC.get("/tracks/" + song.tag, {}, function(sound, error) {
+          if(sound.stream_url) {
+            $('#music').attr('src', sound.stream_url + '?client_id=7af759eb774be5664395ed9afbd09c46');
+            $scope.notStreamable = false;
+            $scope.playTrack();
+          } else {
+            $scope.notStreamable = true;
+            $scope.scUrl = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + sound.id + "&color=0066cc";
+            $scope.$apply()
+          }
+        });
+      }
+      if(song.source == 'YouTube') {
+        // npm package way of streaming the youtube song:
+        // $('#music').attr('src', '/api/youtubes/stream/' + song.tag);
+
+        //iframe api way of streaming the youtube song:
+        player = new YT.Player('youtubePlayer', {
+          height: '500',
+          width: '500',
+          videoId: song.tag.toString(),
+          events: {
+            'onReady': onPlayerReady
+          }
+        });
+        youtubePlaying = true;
+        $scope.playTrack();
+      }
+      if(song.source == 'Tumblr') {
+        $('#music').attr('src', song.audioSource)
+        $scope.playTrack();
+      }
+    }
+
+    //Play and Pause
+    $scope.playTrack = function() {
+      // start music
+      if(!youtubePlaying && !$scope.notStreamable && !currentlyPlaying) {
+        console.log("MUSIC PAUSED: ", music.paused)
+        if (music.paused) {
+          music.play();
+          currentlyPlaying.playing = 'pause';
+          // remove play, add pause
+          pButton.className = "";
+          pButton.className = "pause";
+        } else { // pause music
+          music.pause();
+          // remove pause, add play
+          currentlyPlaying.playing = 'play_arrow';
+          pButton.className = "";
+          pButton.className = "play";
+        }
+      } else if(youtubePlaying) {
+        console.log('TOGGLE YOUTUBE')
+        toggleYoutube();
+      } else {
+        $scope.play(currentlyPlaying, 0)
+      }
+    }
+
+    // Gets audio file duration
+    music.addEventListener("canplaythrough", function () {
+      youtubePlaying ? duration = getDuration() : duration = music.duration;
+    }, false);
+
+    $scope.current = function(){
+      if(player) return player.getCurrentTime();
+    }
+
+    $scope.pause = function(){
+      if(player) player.pauseVideo();
+    }
+
+    $scope.seekTo = function(num) {
+      player.seekTo(25, true);
+    }
+
+    // 4. The API will call this function when the video player is ready.
+    var intervalTest = true;
+    function onPlayerReady(event) {
+      console.log('how many times are we getting in here')
+      if(player.getDuration) duration = player.getDuration();
+      var x = 0;
+      if(intervalTest) {
+        intervalTest = false;
+        setInterval(function() {
+          if(x !== $scope.current()) timeUpdate();
+          x = $scope.current();
+        }, 100);
+      }
+      event.target.playVideo();
       pButton.className = "";
       pButton.className = "pause";
-    } else { // pause music
-      music.pause();
-      // remove pause, add play
-      pButton.className = "";
-      pButton.className = "play";
     }
-  }
 
-  // Gets audio file duration
-  music.addEventListener("canplaythrough", function () {
-    duration = music.duration;
-  }, false);
+    function stopVideo() {
+      if(player) player.stopVideo();
+    }
 
+    function toggleYoutube() {
+      if(player && player.getPlayerState && player.getPlayerState() == 2) { //paused
+        currentlyPlaying.playing = 'pause';
+        pButton.className = "";
+        pButton.className = "pause";
+        youtubePlaying = true;
+        player.playVideo();
+      } else if(player && player.getPlayerState && player.getPlayerState() == 1) { //playing
+        currentlyPlaying.playing = 'play_arrow';
+        pButton.className = "";
+        pButton.className = "play";
+        youtubePlaying = false;
+        $scope.pause();
+      }
+    }
 
+    /// SOUNDCLOUD PLAYER ///
+     var widget = SC.Widget(document.getElementById('soundcloud_widget'));
+     widget.bind(SC.Widget.Events.READY, function() {
+       console.log('Ready...');
+     });
+
+    $scope.scSeekTo = function(num) {
+      widget.seekTo(200000);
+    }
+
+    $scope.scToggle = function() {
+      widget.toggle();
+    }
 
 });
