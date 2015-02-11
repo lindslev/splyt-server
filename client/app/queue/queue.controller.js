93b4367f5ff455a033045184484cdf51a498c9a8
KamillaKhabibrakhmanova <venessiel@gmail.com>
2015-02-10 22:22:49 -0500

    remove youtube call because it blocks login

--- client/app/queue/queue.controller.js
+++ client/app/queue/queue.controller.js
@@ -168,31 +168,6 @@ angular.module('splytApp')
     duration = music.duration;
   }, false);
 
-  $scope.search = function(query) {
-            $scope.query = "";
-            $http({
-                    url: 'https://www.googleapis.com/youtube/v3/search',
-                    method: 'GET',
-                    params: {
-                        part: 'snippet',
-                        q: 'love ' + query,
-                        maxResults: 4
-                    },
-                    headers: {
-                        Authorization: 'Bearer ' + 'AIzaSyC_eZm_iimb5fx5So3Bt4h96ZuKQqd7ARU'
-                    }
-                })
-                .success(function(res) {
-                  console.log(res.items);
-                    $scope.searchResults = res.items;
-                });
-        };
-
-        $scope.search();
-
-
-
-
 
 });
 
