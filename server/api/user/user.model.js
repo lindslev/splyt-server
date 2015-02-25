e0ea5eb1ed86c84d6f00eff84eaf9bea2ee09a93
Emmie Salama <emmie.salama@gmail.com>
2015-02-25 09:35:58 -0500

    made friends feed max 10

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -264,8 +264,7 @@ UserSchema.statics = {
          _.forEach(user.playlist, function(playlist) {
           if (playlist.friend_stream === true) {
            Playlist.findByIdAndUpdate(playlist._id,
-            { $push: { "songs" : song }},
-            { safe: true, upsert: true },
+            { $push:{ "songs" : {$each:[song], $slice: -10} }},
             function( err, model ) {
               cb(err, model);
             }
